da0f6b99afb754bb779c09f23365e213562d2ef6
sanket.gaikwad1 <sanket.gaikwad1@st.ovgu.de>
2024-06-06 23:55:04 +0200

    update1

---
+++ 21Crontab.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#The crontab (cron table) is a configuration file that specifies shell commands to run periodically on a Unix-like operating system. Each line in the crontab file represents a job, and the format is:
+# * * * * * command
+#Each asterisk represents a unit of time, in the following order:
+#
+#Minute (0-59)
+#Hour (0-23)
+#Month (1-12)
+#Day of the week (0-7, where both 0 and 7 represent Sunday)
+
